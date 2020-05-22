7cbfe0657a965e6c51602c23a66303d709d650e0
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2020-05-22 17:38:06 -0300

    Añado socket

---
+++ Socket.cpp
@@ -0,0 +1,136 @@
+#include "Socket.h"
+#include <netdb.h>
+#include <unistd.h>
+#include <cstring>
+#include "OSError.h"
+#include <utility>
+
+#define SEND 0
+#define RECEIVE 1
+#define ERROR -1
+
+Socket::Socket(){
+    num_socket = 0;
+    adress = 0;
+    len_adress = 0;
+    res = 0;
+    free = 0;
+}
+
+Socket::~Socket(){
+    if (free == 1){
+        freeaddrinfo(res);
+    }
+    if (num_socket != -1){
+        shutdown(num_socket,SHUT_RDWR);
+        close(num_socket);
+    }
+}
+
+Socket::Socket(Socket&& other){
+    other.num_socket = this->num_socket;
+    this->num_socket = -1;
+}
+
+Socket&& Socket::operator=(Socket&& other){
+    other.num_socket = this->num_socket;
+    this->num_socket = -1;
+    return std::move(other);
+}
+
+typedef int (*bindorconnect_t)(int socket, const struct sockaddr *direccion,
+                socklen_t len);
+
+//Realiza el trabajo de bindear y conectar, que tienen comportamiento similar
+void Socket::auxBindAndConnect(const char* adress,
+const char* port,bindorconnect_t boc){
+    struct addrinfo hints, *res, *rp;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = 0;
+    int error = 0;
+    if (getaddrinfo(adress,port,&hints,&res) != 0){
+        throw OSError("Fallo en getaddrinfo\n");
+    }                                                                        
+    int sfd;
+    for (rp = res; rp != NULL; rp = rp->ai_next) {
+        sfd = socket(AF_INET, SOCK_STREAM,0);
+        if (sfd == ERROR)
+            continue;         
+        error = boc(sfd,rp -> ai_addr,rp -> ai_addrlen);
+        if (error != ERROR)
+            break;
+        close(sfd);
+    }
+    this -> adress = rp -> ai_addr;
+    this -> len_adress = &(rp -> ai_addrlen);
+    this -> num_socket = sfd;
+    this -> res = rp;
+    this -> free = 1;
+}
+
+void Socket::Bind(const char* adress,const char* port){
+    auxBindAndConnect(adress,port,bind);
+    int val = 1;
+    setsockopt(num_socket, SOL_SOCKET, SO_REUSEADDR,&val, sizeof(val));
+}
+
+void Socket::Connect(const char* adress,const char* port){
+    auxBindAndConnect(adress,port,connect);
+}
+
+//Mi idea principal era hacer un typedef de puntero a funcion
+//Y una funcion auxiliar que hiciera el trabajo comun de enviar y recibir
+//Pero debido a que send y recv tienen firmas diferentes
+//(send tiene un const void* y rec solo void*)
+//No pude hacer el typedef, 
+//por lo que tuve que utilizar un int para elegir la funcion
+int Socket::auxSendAndRecv(char* buffer,size_t len,int send_or_recv){
+    int buffer_size = len;
+    int length = len; 
+    int total = 0;
+    int send_amount = 0;
+    while (length > 0){
+        if (send_or_recv == SEND){
+            send_amount = 
+            send(num_socket,(buffer) + total,buffer_size - total,0);
+        } else {
+            send_amount = 
+            recv(num_socket,(buffer) + total,buffer_size - total,0);
+        }
+        if (send_amount == 0){
+            break;
+        }
+        if (send_amount == ERROR){
+            throw OSError("Error al Enviar o Recibir a través del socket\n");
+        }
+        length -= send_amount;
+        total += send_amount;
+    }
+    return total;
+}
+int Socket::Send(char* buffer,size_t len){
+    return auxSendAndRecv(buffer,len,SEND);
+}
+
+int Socket::Recv(char* buffer,size_t len){
+    return auxSendAndRecv(buffer,len,RECEIVE);
+}
+
+void Socket::Listen(int client_amount){
+    int error = ERROR;
+    while (error == ERROR){
+        error = listen(num_socket,client_amount);
+    }
+}
+
+void Socket::Accept(Socket&& new_socket){
+    //Socket new_socket;
+    int error = ERROR;
+    while (error == ERROR){
+        error = accept(num_socket,adress,len_adress);
+    }
+    new_socket.num_socket = error;
+    //return std::move(new_socket);
+}
---
+++ Socket.h
@@ -0,0 +1,56 @@
+#ifndef CLASS_SOCKET
+#define CLASS_SOCKET
+//#ifndef _POSIX_C_SOURCE
+//#define _POSIX_C_SOURCE 200112L
+//#endif
+#include <sys/socket.h>
+#include <sys/types.h>
+
+typedef int (*bindorconnect_t)(int socket, const struct sockaddr *direccion,
+                socklen_t len);
+
+class Socket {
+private:
+    int num_socket;
+    struct sockaddr* adress;
+    socklen_t* len_adress;
+    struct addrinfo* res;
+    int free;
+    void auxBindAndConnect(const char* adress,
+    const char* port,bindorconnect_t boc);
+    int auxSendAndRecv(char* buffer,size_t len,int send_or_recv);
+public:
+    Socket();
+    Socket(Socket& copy) = delete;
+    Socket& operator=(Socket& copy) = delete;
+    Socket(Socket&& other);
+    Socket&& operator=(Socket&& other);
+    //Realiza una conexión dada una dirección y un puerto
+    //Lanza una excepción en caso de error
+    void Connect(const char* adress,const char* port);
+    //Bindea el socket a una dirección
+    //Lanza una excepción en caso de error
+    void Bind(const char* adress,const char* port);
+    //Envía un mensaje a través del socket, asegurandose de que la totalidad
+    //del mismo llegue a destino
+    //Devuelve la cantidad de bytes enviados
+    //Lanza una excepción en caso de error
+    int Send(char* buffer,size_t len);
+    //Recibe un mensaje de largo len a través del socket, 
+    //y guarda su resultado en el buffer, se asegura de recibir la totalidad
+    //del mensaje
+    //Devuelve la cantidad de bytes recibidos
+    //Lanza una excepción en caso de error
+    int Recv(char* buffer,size_t len);
+    //Espera la conexión de una cantidad de clientes
+    void Listen(int client_amount);
+    //Acepta la conexión de un cliente
+    //Devuelve el socket del nuevo cliente
+    void Accept(Socket&& new_socket);
+    ~Socket();    
+};
+
+
+
+
+#endif
